8cb7cc5d31d33ee0ddff3c0cbab0140bf1be086d
gahyun <s2112@e-mirim.hs.kr>
2023-04-20 14:07:10 +0900

    콘슬 관련 설정 및 수정

--- ConsoleAPP/Program.cs
+++ ConsoleAPP/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleAPP
@@ -10,7 +11,27 @@ namespace ConsoleAPP
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
 
+                if(x%3 == 0)
+                {
+                    Console.WriteLine("__@");
+
+                } else if (x % 3 == 1)
+                {
+                    Console.WriteLine("_^@");
+
+                } else
+                {
+                    Console.WriteLine("^_@");
+                }
+                Thread.Sleep(1000);
+                x++;
+            }
         }
     }
 }
